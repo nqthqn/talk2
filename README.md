da8f4a6f2c35236343c2ea4805bb21c58707b895
Michael Herman <hermanmu@gmail.com>
2014-06-06 11:19:31 -0500

    fixed django project structure, added AJAX, setup sample AJAX

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-talk
-====
+# talk
+
 
 A small ajax powered Django app for implementing a super simple communication wall for posting and commenting.
+
+
+## URLs
+
+1. Main - http://localhost:8000/talk/
---
+++ talk_project/talk_project/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from talk import views
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns('', 
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^talk/', include('talk.urls')),  # talk urls
+)

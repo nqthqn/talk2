7dc9048aaf98d94482a64e32485e5bc960608c3d
Nate Nichols <broinjc@gmail.com>
2014-06-09 08:35:21 -0700

    Wrote views for creating posts and comments. Refactored main.js.

--- talk_project/talk_project/urls.py
+++ talk_project/talk_project/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
 from talk import views
 
+from .views import logout_page
+
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('', 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', 'django.contrib.auth.views.login'),
+    (r'^logout/$', logout_page),
     url(r'^', include('talk.urls')),
 )
